7dd71367950988d0d6d06df1bc57516017507b91
Oscar <oscar.manrique@ibm.com>
2022-09-19 23:14:09 -0500

    final version

---
+++ code/Python/cat_ws_1/build/hello_turtle/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/juan/cat_ws_1/src/hello_turtle
+# Build directory: /home/juan/cat_ws_1/build/hello_turtle
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
